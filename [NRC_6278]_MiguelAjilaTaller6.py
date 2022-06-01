0bb640e17844261fc7ad4a81f2cbb3d7c9f5cdf8
MiguelAjila <105440981+MiguelAjila@users.noreply.github.com>
2022-05-31 19:01:49 -0500

    Update [NRC_6278]_MiguelAjilaTaller6.py
    Creación de clase grafo

--- [NRC_6278]_MiguelAjilaTaller6.py
+++ [NRC_6278]_MiguelAjilaTaller6.py
@@ -2,3 +2,17 @@
 #Importamos la libreria fifo "queue"
 from queue import Queue
 #Se debe crear funciones contienen metodos y variables
+class Grafo:
+    """
+    Se crea una clase denominada Grafo, esta permite crear diferentes metodos y variables.
+    Se implemento el contructor __init__
+    con ayuda de un "def" se crea un metodo, utilizando la variable "num_nodos".
+    El uso del comando self vincula los datos y se los agrega aun arreglo o tambien denominado lista.
+    --------------------------------------------------------------------------------------------------------------
+    El siguiente codigo tiene la funcion de un diccionario
+
+    def __init__(self, num_nodos, direccion=True):
+        self.m_num_nodos = num_nodos
+        self.m_nodos = range(self.m_num_nodos)
+
+    """
